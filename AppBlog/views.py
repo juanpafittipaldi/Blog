590e3063093a3e4f85b7adfda9fc913212890b1c
juanpafittipaldi <juanpafitti@gmail.com>
2022-09-25 21:53:16 -0300

    arreglados errores login y register

--- AppBlog/views.py
+++ AppBlog/views.py
@@ -3,21 +3,21 @@ from django.shortcuts import render
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login, logout, authenticate
 from AppBlog.forms import UserRegisterForm
-from django.http import HttpResponse
+
 # Create your views here.
 
 def login_request(request):
-    if request.method=="POST":
-        form=AuthenticationForm(request.POST)
+    if request.method== "POST":
+        form=AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             usu=request.POST["username"]
             clave=request.POST["password"]
             usuario=authenticate(username=usu, password=clave)
             if usuario is not None:
                 login(request, usuario)
-                return render(request, "inicio.html", {"mensaje":f"Bienvenido {usuario}"})
+                return render(request, "AppBlog/logueado.html", {"mensaje":f"Bienvenido {usuario}"})
             else:
-                return render(request, "AppBlog/login.html",{"formulario":form, "mensaje":"usuario o contaseña incorrectos"})
+                return render(request, "AppBlog/login.html",{"formulario":form, "mensaje":"usuario o contaseña incorrectos 2"})
         else:
             return render(request, "AppBlog/login.html",{"formulario":form, "mensaje":"usuario o contaseña incorrectos"})
     else:
@@ -25,13 +25,15 @@ def login_request(request):
         return render(request, "AppBlog/login.html",{"formulario":form})
 
 def register(request):
-    if request.method=="POST":
-        form=UserRegisterForm(request.method)
+    if request.method == "POST":
+        form=UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            return render(request,"AppBlog/login.html",{"mensaje":"Usuario creado correctamente"})
+            formlogin=AuthenticationForm()
+            return render(request,"AppBlog/login.html",{"formulario":formlogin,"mensaje":"Usuario creado correctamente"})
         else:
-            return render(request,"AppBlog/register.html",{"mensaje":"Error al registrarse, vuelva a intentarlo"})
+            form=UserRegisterForm()
+            return render(request,"AppBlog/register.html",{"formulario":form,"mensaje":"Error al registrarse, vuelva a intentarlo"})
     else:
         form=UserRegisterForm()
         return render(request, "AppBlog/register.html",{"formulario":form})
